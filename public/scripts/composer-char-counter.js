d30321de7b4d400e1270972c17e04347ee187c4a
Sara Irving <sarakirving@gmail.com>
2020-07-14 21:08:31 +0000

    Added eventHandler that fires when there is a keydown in the testarea. It keeps track of the characters written and displays how much space is left via the counter. When you've gone over the max allowed characters the counter displays a negative value in red

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Scripts loaded!");
+  
+  $('textarea').keydown(function () {
+    const value = $(this).val().length;
+    const maxValue = 140;
+    const counter = $(this).parent().find('.counter');
+
+    counter.val(maxValue - value);
+    counter.val() < 0 ?  counter.css("color", "red") : counter.css("color", "black");
+  })
+
+
+
+});
