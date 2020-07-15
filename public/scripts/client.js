f0eb49ad12b5ec7f3d0673a1daf1a945f1455d51
Sara Irving <sarakirving@gmail.com>
2020-07-15 16:25:16 +0000

    Updated target elements to correct value so that tweets are now being added to the page in the browser

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -18,10 +18,36 @@ const tweetData = {
   "created_at": 1461116232227
 }
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 $(document).ready(function() {
 
   const createTweetElement = function (tweetData) {
-    const article = `
+    //create the $tweet element
+    let $tweet = `
       <article class="tweet-article">
         <header class="tweet-header">
           <div class="display-info">
@@ -42,9 +68,20 @@ $(document).ready(function() {
       </article>
         `;
 
-    return article;
+    return $tweet;
   }
 
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (tweetObj of tweets) {
+      let $createdTweet = createTweetElement(tweetObj);
+      console.log($createdTweet);
+      $('.posted-tweet').append($createdTweet);
+    }
+
+  }
 
 
 
@@ -55,6 +92,9 @@ $(document).ready(function() {
 // Test / driver code (temporary)
   console.log($tweet); // to see what it looks like
 
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  $('.posted-tweet').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  renderTweets(data);
+
 
 });
