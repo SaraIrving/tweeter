6a448314820c4ff601ebd066cbe5cd9209889a17
Sara Irving <sarakirving@gmail.com>
2020-07-17 03:01:55 +0000

    Added comments and fixed syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  //alerts that scripts have loaded
   console.log("Scripts loaded!");
   
-  $("#tweet-text").keydown(function () {
+  //keeps track of how many characters the tweet being typed contains
+  $("#tweet-text").keydown(function() {
     const value = $(this).val().length;
     const maxValue = 140;
     const counter = $(this).parent().find('.counter');
 
     counter.val(maxValue - value);
     counter.val() < 0 ?  counter.css("color", "red") : counter.css("color", "black");
-  })
-
-
+  });
 
 });
